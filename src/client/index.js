3bd0a71c7f7edf172c09f0b15968629673a38540
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-01-26 21:32:35 -0500

    :construction: Work in Progress: Fix bundle setup

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,24 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
+
+import "./styles/about.scss";
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+
+import "fonts/JapaneseRobot-0WWdX.ttf";
+
+import "images/data-processing-image.svg";
+import "images/facebook-logo.svg";
+import "images/instagram-logo.svg";
+import "images/pinterest-logo.svg";
+import "images/tiktok-logo.svg";
+import "images/twitter-logo.svg";
+import "images/youtube-logo.svg";
 
 console.log(checkForName);
 
-alert("I EXIST")
+alert("I EXIST");
 console.log("CHANGE!!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,1 @@
-const path = require("path");
-const webpack = require("webpack");
-module.exports = {
-  entry: "./src/client/index.js",
-};
+

8211c1a1103b9ef3d7ed80a0e1158004311275d0
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-01-29 08:44:50 -0500

    :recycle: Refactor Code: Change js files

--- src/server/index.js
+++ src/server/index.js
@@ -32,8 +32,7 @@ app.use(express.static("dist"));
 console.log(__dirname);
 
 app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve("src/client/views/index.html"));
+  res.sendFile("dist/index.html");
 });
 
 // Setup server

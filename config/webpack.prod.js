2cb4b89b03af563882019d34f7f940c32a266971
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-01-30 22:30:49 -0500

    :wrench: Change Config: Update dependencies and configs

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,23 +1,61 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+  entry: "./src/client/index.js",
+  devtool: "source-map",
+  mode: "production",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: "file-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              encoding: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: "Home",
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+  ],
+};

2da8807fdeab9393ac8e57d893d2cd0c62760213
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-02-03 08:39:13 -0500

    :wrench: Change Config: Update dev config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -8,7 +8,8 @@ module.exports = {
   mode: "development",
   devtool: "source-map",
   devServer: {
-    inline: true,
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
     port: 8080,
   },
   module: {
@@ -42,11 +43,8 @@ module.exports = {
       filename: "./index.html",
     }),
     new CleanWebpackPlugin({
-      // Simulate the removal of files
       dry: true,
-      // Write Logs to Console
       verbose: true,
-      // Automatically remove all unused webpack assets on rebuild
       cleanStaleWebpackAssets: true,
       protectWebpackAssets: false,
     }),

d52d0f38bc3ed1608dc740b61a82c3e86a409daf
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-02-05 20:07:11 -0500

    :white_check_mark: Update Test: Update url validator

--- src/client/js/validURL.js
+++ src/client/js/validURL.js
@@ -1,14 +1,11 @@
 function validURL(str) {
-  var pattern = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-      "(\\#[-a-z\\d_]*)?$",
-    "i"
-  ); // fragment locator
-  return !!pattern.test(str);
+  var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+  if (!regex.test(str)) {
+    alert("Please enter valid URL.");
+    return false;
+  } else {
+    return true;
+  }
 }
 
 export { validURL };

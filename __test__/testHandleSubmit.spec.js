d00dcdd7436c8ce4630678829d7df47ecbaec159
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-02-06 10:22:43 -0500

    :white_check_mark: Update Tests: Add Jest tests for JS scripts

---
+++ __test__/testHandleSubmit.spec.js
@@ -0,0 +1,8 @@
+import { runAction } from "../src/client/js/formHandler";
+
+// To check if 'handleSubmit' produces the right output.
+describe("Testing the submit functionality", () => {
+  test("Testing the handleSubmit() function", () => {
+    expect(runAction).toBeDefined();
+  });
+});

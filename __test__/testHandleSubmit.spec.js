1d53edf6e8467f42da7fb73e3011b267a63f7684
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-02-17 15:17:03 -0500

    :pencil2: Fix Typo: Remove typo from a 'test'

--- __test__/testHandleSubmit.spec.js
+++ __test__/testHandleSubmit.spec.js
@@ -2,7 +2,7 @@ import { runAction } from "../src/client/js/formHandler";
 
 // To check if 'handleSubmit' produces the right output.
 describe("Testing the submit functionality", () => {
-  test("Testing the handleSubmit() function", () => {
+  test("Testing the runAction() function", () => {
     expect(runAction).toBeDefined();
   });
 });
